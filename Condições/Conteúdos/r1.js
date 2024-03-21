bfb71b80083540c4742a0237ec65d643c22b2d2b
Washington Medeiros <wmedeirosm001@gmail.com>
2024-03-21 19:33:58 -0300

    Atividade 1

---
+++ Condições/Conteúdos/r1.js
@@ -0,0 +1,9 @@
+console.log('O comando funcionou perfeitamente!')
+
+var velocidade = 60.5
+console.log(`A velocidade do seu varo é ${velocidade} Km/h` )
+
+if (velocidade > 70) {
+    console.log('Voce ultrapassou a velocidade permitida da via! MULTADO')
+}
+console.log('Dirija sempre usando o sinto de segurança!')
