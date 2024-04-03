27fbfeab57185427448472cd88a23be391ac9b69
Washington Medeiros <wmedeirosm001@gmail.com>
2024-04-03 15:02:43 -0300

    Switch

---
+++ Condições/Conteúdos/SWITCH.js
@@ -0,0 +1,21 @@
+// Condição simples (só if)
+// Condição Composta (if e else)
+// COndição multipla
+/*
+
+    switch(expressao) {
+        
+        case valor  1:
+            console.log('Amarelo')
+            break
+        case valor 2:
+            console.log('Azul')
+            break
+        case valor 3:
+            console.log('VErmelho')
+            break
+        default // se nenhum for satisfeito
+        console.log('VErde')
+            break
+    }
+*/
