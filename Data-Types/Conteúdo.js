4e53e8270281aa25e28bfb437b86b6bc10f4d6c8
Washington Medeiros <wmedeirosm001@gmail.com>
2024-05-21 08:41:11 -0300

    Data types content

---
+++ Data-Types/Conteúdo.js
@@ -0,0 +1,18 @@
+/* 
+Number 
+    Nan
+    Infinit
+String
+Boolen
+Null
+Undefined
+Object 
+    Array
+Funvtion
+
+*/
+
+// Para ver o tipo da variável: typeof(variável)
+
+// x.toLocaleString('pt-BR',{style: 'currencey','currency:'EUR})
+
