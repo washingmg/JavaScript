0c60fdb2a093b600064ca1b380461642f2c121f3
Washington Medeiros <wmedeirosm001@gmail.com>
2024-05-22 15:27:16 -0300

    Operator content

---
+++ Operadores/main.js
@@ -0,0 +1,26 @@
+// Os operadores aritmétricos tem ordem de porcedência
+//  1° (), 2° **, 3° *, /, % 'O mais a esquerda começa primeiro', 4° +, -
+
+// x += 5 é a mesma coisa que ++ e x  -= 5 é a mesma coisa que --
+
+/*
+ = -> x = 4, x recebe 4
+ == -> x == 4, x é a mesma coisa que 4, se são do mesmo valor, independente do tipo
+ === -> 5 === '5' retorna False, se são do mesmo valor e tipo
+
+Operadores lógicos:
+  ! -> negação, não
+  && -> Conjunção, 'e' tem que ser os dois
+  || -> disjunção, 'ou' um ou outro serve
+
+
+Operador Ternário:   média > 7 ? 'Aprovador : 'Reprovado'
+                                'if'         'else'
+
+Ordem de procedência
+
+1° Operadores aritmétricos (), ** ... 
+2° Operadores racionais <, >, <= ...
+3° Operadores lógicos 1° !, 2° &&, 3° ||
+
+*/
